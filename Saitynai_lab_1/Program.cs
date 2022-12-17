0f0cd881a57f80696f6662947f39ff5331758c92
Simona <bliuv.simona@gmail.com>
2022-12-17 15:26:14 +0200

    Added Cors

--- Saitynai_lab_1/Program.cs
+++ Saitynai_lab_1/Program.cs
@@ -15,6 +15,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
+var MyAllowSpecificOrigins = "_myAllowCpecificOrigins";
+
 builder.Services.AddControllers();
 
 builder.Services.AddIdentity<BookUser, IdentityRole>()
@@ -49,12 +51,23 @@ builder.Services.AddAuthorization(options =>
 
 builder.Services.AddSingleton<IAuthorizationHandler, ResourceOwnerAuthorizationHandler>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("*");
+        });
+});
+
 var app = builder.Build();
 
 app.UseRouting();
 app.MapControllers();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 using var scope = app.Services.CreateScope();
 var dbContext = scope.ServiceProvider.GetRequiredService<BooksDbContext>();
