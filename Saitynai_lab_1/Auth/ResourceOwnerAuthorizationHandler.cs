3b93239d1022ead4c85518c14ab0800ad20f0e78
Patossium <bliuv.simona@gmail.com>
2022-12-18 22:12:57 +0200

    Revert "Try again"
    This reverts commit a01b32049e273374fbf91eb7e02517c20bfcde9e.

--- Saitynai_lab_1/Auth/ResourceOwnerAuthorizationHandler.cs
+++ Saitynai_lab_1/Auth/ResourceOwnerAuthorizationHandler.cs
@@ -4,19 +4,21 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.JsonWebTokens;
 
 
-namespace DemoRestSimonas.Auth
+namespace DemoRestSimonas.Auth;
+
+public class ResourceOwnerAuthorizationHandler : AuthorizationHandler<ResourceOwnerRequirement, IUserOwnedResource>
 {
-    public class ResourceOwnerAuthorizationHandler : AuthorizationHandler<ResourceOwnerRequirement, IUserOwnedResource>
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ResourceOwnerRequirement requirement,
+        IUserOwnedResource resource)
     {
-        List<string> tokenBlackList = new List<string>();
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ResourceOwnerRequirement requirement, IUserOwnedResource resource)
+        if (context.User.IsInRole(BookRoles.Admin) ||
+            context.User.FindFirstValue(JwtRegisteredClaimNames.Sub) == resource.UserId)
         {
-            if (context.User.IsInRole(BookRoles.Admin) || context.User.FindFirstValue(JwtRegisteredClaimNames.Sub) == resource.UserId)
-            {
-                context.Succeed(requirement);
-            }
-            return Task.CompletedTask;
+            context.Succeed(requirement);
         }
+
+        return Task.CompletedTask;
     }
-    public record ResourceOwnerRequirement : IAuthorizationRequirement;
 }
+
+public record ResourceOwnerRequirement : IAuthorizationRequirement;
